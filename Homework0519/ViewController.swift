50470eaa1d34a4722141363158c9d7bb7292bd22
winni <bnhg093@hotmali.com>
2018-05-19 20:12:58 +0800

    Initial Commit

---
+++ Homework0519/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Homework0519
+//
+//  Created by winni on 2018/5/19.
+//  Copyright © 2018年 winni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
